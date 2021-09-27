4e41a8f2826c8e3f1e43db708d444b668f9178f5
LuisOlan <60374196+LuisOlan@users.noreply.github.com>
2021-09-27 17:35:01 -0500

    Ionic-basic-1989942

---
+++ Ionic-basic-1989942/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic-basic-1989942',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
